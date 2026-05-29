c701eb4aaa249f512d62925938d1e52146071bbf
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 00:14:19 +0500

    Add: 3) 1..100 oralig‘ida guess [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/3__1__100_oralig_ida_guess.py
@@ -0,0 +1,11 @@
+yashirin = 42
+while True:
+    a = int(input())
+    if a < yashirin:
+        print("Low")
+    elif a > yashirin:
+        print("High")
+    else:
+        print("Correct")
+        break
+            
