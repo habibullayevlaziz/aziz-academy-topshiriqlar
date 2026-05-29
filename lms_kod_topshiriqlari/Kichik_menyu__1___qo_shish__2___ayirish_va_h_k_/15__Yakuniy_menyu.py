1a4c2111469517d18b690786aa40c010911a5f7e
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 00:08:17 +0500

    Add: 15) Yakuniy menyu [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Kichik_menyu__1___qo_shish__2___ayirish_va_h_k_/15__Yakuniy_menyu.py
@@ -0,0 +1,21 @@
+while True:
+    a, b = map(int, input().split())
+    amal = int(input())
+    if amal == 0:
+        break
+    elif amal == 1:
+        print(a + b)
+    elif amal == 2:
+        print(a - b)
+    elif amal == 3:
+        print(a * b)
+    elif amal == 4:
+        print(a / b)
+    elif amal == 5:
+        print(a % b)
+    elif amal == 6:
+        print(a ** b)
+    x = int(input())
+    if x == 0:
+        print("Exit")
+        break
