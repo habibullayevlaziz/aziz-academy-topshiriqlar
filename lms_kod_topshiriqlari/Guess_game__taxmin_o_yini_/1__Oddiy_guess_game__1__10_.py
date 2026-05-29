d82e552b1ce6001443b41b093941c7ddb0e56628
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 00:14:14 +0500

    Add: 1) Oddiy guess game (1..10) [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/1__Oddiy_guess_game__1__10_.py
@@ -0,0 +1,10 @@
+yashirin = 7
+while True:
+    a = int(input())
+    if a < yashirin:
+        print("Low")
+    elif a > yashirin:
+        print("High")
+    else:
+        print("Correct")
+        break
