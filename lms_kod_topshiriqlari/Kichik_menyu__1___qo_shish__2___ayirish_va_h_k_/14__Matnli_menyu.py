b80a5d224e11fb8357325f5e27865d5e90844e30
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-29 21:28:58 +0500

    Add: 14) Matnli menyu [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Kichik_menyu__1___qo_shish__2___ayirish_va_h_k_/14__Matnli_menyu.py
@@ -0,0 +1,10 @@
+a, b = map(int, input().split())
+amal = input()
+if amal == "add":
+    print(a + b)
+elif amal == "sub":
+    print(a - b)
+elif amal == "mul":
+    print(a * b)
+elif amal == "div":
+    print(a / b)
