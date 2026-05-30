8f195f8198c5f1175faaa17f87fdfc5819192ab5
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 12:15:40 +0500

    Add: 3) Difference: A - B [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Set_operatsiyalari__union__intersection__differenc/3__Difference__A_-_B.py
@@ -0,0 +1,8 @@
+a = set(map(int, input().split()))
+b = set(map(int, input().split()))
+difference_set = a.difference(b)
+if not difference_set:
+    print("BO'SH")
+else:
+    result = sorted(difference_set)
+    print(*(result))
