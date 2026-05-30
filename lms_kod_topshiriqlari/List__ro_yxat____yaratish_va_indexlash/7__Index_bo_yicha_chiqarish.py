80a992f8c283e148e1810892ecc1227a93ef265e
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 13:49:39 +0500

    Add: 7) Index bo‘yicha chiqarish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List__ro_yxat____yaratish_va_indexlash/7__Index_bo_yicha_chiqarish.py
@@ -0,0 +1,7 @@
+n = int(input())
+lst = list(map(int, input().split()))
+print(lst[0])
+print(lst[2])
+# n = int(input())
+# lst = list(map(int, input().split()))
+# 0 va 2-indexdagi elementlarni chiqaring.
