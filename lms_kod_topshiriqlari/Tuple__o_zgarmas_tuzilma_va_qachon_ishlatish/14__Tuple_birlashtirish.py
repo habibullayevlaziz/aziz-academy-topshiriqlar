713e9adb6325fb05f3046a40cfefcb6cad997d9a
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 08:52:05 +0500

    Add: 14) Tuple birlashtirish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Tuple__o_zgarmas_tuzilma_va_qachon_ishlatish/14__Tuple_birlashtirish.py
@@ -0,0 +1,11 @@
+n1 = int(input())
+t1 = tuple(map(int, input().split()))
+n2 = int(input())
+t2 = tuple(map(int, input().split()))
+birlashgan_t = t1 + t2
+print(birlashgan_t)
+# n1 = int(input())
+# t1 = tuple(map(int, input().split()))
+# n2 = int(input())
+# t2 = tuple(map(int, input().split()))
+# Ikkalasini birlashtirib chiqaring.
