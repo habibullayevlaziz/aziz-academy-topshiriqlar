3bb94467a64060429d7877313971b0b9f9a2980a
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 13:54:57 +0500

    Add: 10) Birinchi va oxirgi yig‘indisi [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List__ro_yxat____yaratish_va_indexlash/10__Birinchi_va_oxirgi_yig_indisi.py
@@ -0,0 +1,6 @@
+n = int(input())
+lst = list(map(int, input().split()))
+print(lst[0] + lst[-1])
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Birinchi va oxirgi element yig'indisini chiqaring.
