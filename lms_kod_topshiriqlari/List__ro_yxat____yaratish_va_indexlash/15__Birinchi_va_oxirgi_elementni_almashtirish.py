de2d75bebaeb126ccc25eb8d23d068eb2f159ba4
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 20:33:08 +0500

    Add: 15) Birinchi va oxirgi elementni almashtirish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List__ro_yxat____yaratish_va_indexlash/15__Birinchi_va_oxirgi_elementni_almashtirish.py
@@ -0,0 +1,7 @@
+n = int(input())
+lst = list(map(int, input().split()))
+lst[0], lst[-1], = lst[-1], lst[0]
+print(lst)
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Birinchi va oxirgi elementni joyini almashtiring va listni chiqaring.
