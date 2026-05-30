973054c740cc49f9b6cb1d5d65047e2d59e69110
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 08:52:02 +0500

    Add: 12) Tuple yig‘indisi [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Tuple__o_zgarmas_tuzilma_va_qachon_ishlatish/12__Tuple_yig_indisi.py
@@ -0,0 +1,7 @@
+n = int(input())
+t = tuple(map(int, input().split()))
+yigindi = sum(t)
+print(yigindi)
+# n = int(input())
+# t = tuple(map(int, input().split()))
+# Elementlar yig'indisini chiqaring.
