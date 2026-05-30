cf034e1b0a29336a925ee073561d501cdd5c80f0
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 08:58:56 +0500

    Add: 2) values() metodi [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Dict_metodlari__keys__values__items/2__values___metodi.py
@@ -0,0 +1,13 @@
+n = int(input())
+d = {}
+for _ in range(n):
+    k, v = input().split()
+    d[k] = int(v)
+qiymatlar_royxati = list(d.values())
+print(qiymatlar_royxati)
+# n = int(input())
+# d = {}
+# for _ in range(n):
+#     k, v = input().split()
+#     d[k] = int(v)
+# qiymatlarni list qilib chiqaring
