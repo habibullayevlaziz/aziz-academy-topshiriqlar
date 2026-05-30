532d9deecd2b1b54f8c29da418a40f6c62dc3edd
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 20:23:36 +0500

    Add: 12) Listni teskari index bilan [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List__ro_yxat____yaratish_va_indexlash/12__Listni_teskari_index_bilan.py
@@ -0,0 +1,7 @@
+n = int(input())
+lst = list(map(int, input().split()))
+print(lst[-1])
+print(lst[0])
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Oxirgi va birinchi elementni manfiy va musbat index bilan chiqaring.
