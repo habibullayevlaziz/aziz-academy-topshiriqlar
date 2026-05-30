5e412a9185e0c2fa7571884a15ff278af926187b
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 08:59:00 +0500

    Add: 3) items() metodi [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Dict_metodlari__keys__values__items/3__items___metodi.py
@@ -0,0 +1,13 @@
+n = int(input())
+d = {}
+for _ in range(n):
+    k, v = input().split()
+    d[k] = int(v)
+items_royxatlar = list(d.items())
+print(items_royxatlar)    
+# n = int(input())
+# d = {}
+# for _ in range(n):
+#     k, v = input().split()
+#     d[k] = int(v)
+# items ni list qilib chiqaring
