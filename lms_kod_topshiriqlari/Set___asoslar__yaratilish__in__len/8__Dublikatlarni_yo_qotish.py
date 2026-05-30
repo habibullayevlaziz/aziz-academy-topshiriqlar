2b5265927661b93654d3b1de2618e5b896f7c1bd
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 12:09:13 +0500

    Add: 8) Dublikatlarni yo‘qotish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Set___asoslar__yaratilish__in__len/8__Dublikatlarni_yo_qotish.py
@@ -0,0 +1,6 @@
+n = int(input())
+nums = list(map(int, input().split()))
+print(len(set(nums))) 
+# n = int(input())
+# nums = list(map(int, input().split()))
+# nechta unikal element borligini chiqaring
