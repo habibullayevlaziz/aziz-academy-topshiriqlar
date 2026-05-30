b69c8366fb9bd7224d087f3143485df355df65af
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 08:57:46 +0500

    Add: 14) Ko‘p qiymatni * bilan yig‘ish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Tuple_unpacking__tuple_yechish_/14__Ko_p_qiymatni___bilan_yig_ish.py
@@ -0,0 +1,9 @@
+n = int(input())
+sonlar = map(int, input().split())
+birinchi, *qolganlari = sonlar
+print(birinchi)
+print(qolganlari)
+# n va n ta son beriladi.
+# Birinchi sonni alohida oling.
+# Qolganlarini list qilib oling.
+# Ikkalasini chiqaring.
