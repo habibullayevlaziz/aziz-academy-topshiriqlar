63d6ab8f74390ae1e89ddf3ab8592946a8b9ee95
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 08:45:11 +0500

    Add: 10) Cheksiz loop xavfsiz variant [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Infinite_loop_tushunchasi_va_oldini_olish/10__Cheksiz_loop_xavfsiz_variant.py
@@ -0,0 +1,10 @@
+while True:
+    t = input()
+    if t == "1":
+        print("1")
+    elif t == "2":
+        print("2")
+    elif t == "3":
+        print("3")
+    elif t == "0":
+        break
