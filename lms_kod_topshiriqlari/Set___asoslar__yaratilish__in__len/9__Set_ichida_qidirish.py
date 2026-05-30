5468d9e16e7301a410c7a2342fde0dce95818999
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 12:09:17 +0500

    Add: 9) Set ichida qidirish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Set___asoslar__yaratilish__in__len/9__Set_ichida_qidirish.py
@@ -0,0 +1,9 @@
+s = {'python', 'java', 'c++'}
+q = input().strip()
+if q in s:
+    print("OK")
+else:
+    print("NO")
+# s = {'python', 'java', 'c++'}
+# q = input().strip()
+# 's' ichida bo'lsa OK, bo'lmasa NO chiqaring
