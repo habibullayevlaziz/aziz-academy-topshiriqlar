b4100492c2400f0de4748900ce367a496d4f9283
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 13:27:21 +0500

    Add: 12) break qayerda ishlaydi? [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Infinite_loop_tushunchasi_va_oldini_olish/12__break_qayerda_ishlaydi_.py
@@ -0,0 +1,11 @@
+n = int(input())
+break_at = int(input())
+i = 1
+while i < n:
+    if i == break_at:
+        break
+    print(i)
+    i += 1
+# n = int(input())
+# break_at = int(input())
+# for yoki while ichida, break_at ga kelganda to'xtating.
