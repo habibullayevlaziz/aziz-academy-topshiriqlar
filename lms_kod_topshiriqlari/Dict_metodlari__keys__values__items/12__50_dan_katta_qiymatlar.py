4d2fc9ed1e5e827cf155e7780d98cadafc3e9a96
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 09:00:14 +0500

    Add: 12) 50 dan katta qiymatlar [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Dict_metodlari__keys__values__items/12__50_dan_katta_qiymatlar.py
@@ -0,0 +1,15 @@
+n = int(input())
+d = {}
+for _ in range(n):
+    k, v = input().split()
+    d[k] = int(v)
+for v in d.values():
+    if v > 50:
+        print(v)
+
+    # n = int(input())
+# d = {}
+# for _ in range(n):
+#     k, v = input().split()
+#     d[k] = int(v)
+# 50 dan kattalarini chiqaring
