fb5ee32513f2a41ef432ed2f41386498b0e049a7
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 08:59:04 +0500

    Add: 5) Qiymatlarni chiqarish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Dict_metodlari__keys__values__items/5__Qiymatlarni_chiqarish.py
@@ -0,0 +1,13 @@
+n = int(input())
+d = {}
+for _ in range(n):
+    k, v = input().split()
+    d[k] = int(v)
+for value in d. values():
+    print(value)
+# n = int(input())
+# d = {}
+# for _ in range(n):
+#     k, v = input().split()
+#     d[k] = int(v)
+# qiymatlarni chiqaring
