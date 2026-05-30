2836d5e85d030c175da9396ab9f0058f539ef67c
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 13:52:50 +0500

    Add: 9) Index orqali o‘zgartirish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List__ro_yxat____yaratish_va_indexlash/9__Index_orqali_o_zgartirish.py
@@ -0,0 +1,7 @@
+n = int(input())
+lst = list(map(int, input().split()))
+lst[1] = 20
+print(lst)
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Ikkinchi elementni 20 ga o'zgartiring va listni chiqaring.
