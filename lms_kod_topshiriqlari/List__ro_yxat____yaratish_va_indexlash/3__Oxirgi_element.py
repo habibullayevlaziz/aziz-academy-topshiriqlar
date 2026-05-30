d16db734affe32b513f23721090465a1c032596b
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 13:45:05 +0500

    Add: 3) Oxirgi element [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List__ro_yxat____yaratish_va_indexlash/3__Oxirgi_element.py
@@ -0,0 +1,6 @@
+n = int(input())
+lst = list(map(int, input().split()))
+print(lst[-1])
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Oxirgi elementni manfiy index bilan chiqaring.
