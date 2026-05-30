924c1e92465163c7d1639484fd19d392bb1f3ea8
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 20:28:47 +0500

    Add: 13) Index mavjudligini tekshirish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List__ro_yxat____yaratish_va_indexlash/13__Index_mavjudligini_tekshirish.py
@@ -0,0 +1,9 @@
+n = int(input())
+lst = list(map(int, input().split()))
+if len (lst) > 5:
+    print(lst[5])
+else:
+    print("Error")
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Agar index 5 mavjud bo'lsa elementni, bo'lmasa 'Error' chiqaring.
