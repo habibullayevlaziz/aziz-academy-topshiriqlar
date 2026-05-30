bba521f475af983f3c4b9ce0cec2b05b7388bcdc
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 08:51:51 +0500

    Add: 9) Listdan tuple [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Tuple__o_zgarmas_tuzilma_va_qachon_ishlatish/9__Listdan_tuple.py
@@ -0,0 +1,7 @@
+n = int(input())
+lst = list(map(int, input().split()))
+t = tuple(lst)
+print(t)
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Uni tuple ga o'tkazing va chiqaring.
