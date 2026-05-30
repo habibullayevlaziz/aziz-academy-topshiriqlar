e12c1832f6d0d03a44e9a0331947fd025cbd4651
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 08:58:55 +0500

    Add: 1) keys() metodi [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Dict_metodlari__keys__values__items/1__keys___metodi.py
@@ -0,0 +1,13 @@
+n = int(input())
+d = {}
+for _ in range(n):
+    k, v = input().split()
+    d[k] = int(v)
+kalitlar_royxati = list(d.keys())
+print(kalitlar_royxati)
+# n = int(input())
+# d = {}
+# for _ in range(n):
+#     k, v = input().split()
+#     d[k] = int(v)
+# kalitlarni list qilib chiqaring
