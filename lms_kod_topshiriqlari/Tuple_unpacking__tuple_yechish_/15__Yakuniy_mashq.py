338cbfbb9b730fe9665d320814ae7bf7f5106faa
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-30 08:58:25 +0500

    Add: 15) Yakuniy mashq [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/Tuple_unpacking__tuple_yechish_/15__Yakuniy_mashq.py
@@ -0,0 +1,10 @@
+n = int(input())
+sonlar = list(map(int, input().split()))
+birinchi, *ortadagilar, oxirgi = sonlar
+print(birinchi)
+print(ortadagilar)
+print(oxirgi)
+# Uchta son beriladi.
+# Birinchi va oxirgisini alohida oling.
+# O‘rtadagilarni list qilib oling.
+# Hammasini chiqaring.
