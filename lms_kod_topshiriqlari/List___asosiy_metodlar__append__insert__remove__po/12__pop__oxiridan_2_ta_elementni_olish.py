73f0de25ba4499bafb4772e7ebec08293a4f338c
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-31 22:31:12 +0500

    Add: 12) pop: oxiridan 2 ta elementni olish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List___asosiy_metodlar__append__insert__remove__po/12__pop__oxiridan_2_ta_elementni_olish.py
@@ -0,0 +1,11 @@
+n = int(input())
+lst = list(map(int, input().split()))
+a = lst.pop()
+b = lst.pop()
+print(a)
+print(b)
+print(lst)
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Oxiridan 2 ta elementni ketma-ket pop qiling va chiqaring.
+# Oxirida qolgan listni chiqaring.
