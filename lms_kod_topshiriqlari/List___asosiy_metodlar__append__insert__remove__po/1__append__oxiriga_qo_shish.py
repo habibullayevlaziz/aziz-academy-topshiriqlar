1020ea662553dc1cf8e9a85eadd24dc6e843b89e
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-31 22:15:52 +0500

    Add: 1) append: oxiriga qo‘shish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List___asosiy_metodlar__append__insert__remove__po/1__append__oxiriga_qo_shish.py
@@ -0,0 +1,9 @@
+n = int(input())
+lst = list(map(int, input().split()))
+val = int(input())
+lst.append(val)
+print(lst)
+# n = int(input())
+# lst = list(map(int, input().split()))
+# val = int(input())
+# Oxiriga val ni append qiling va listni chiqaring.
