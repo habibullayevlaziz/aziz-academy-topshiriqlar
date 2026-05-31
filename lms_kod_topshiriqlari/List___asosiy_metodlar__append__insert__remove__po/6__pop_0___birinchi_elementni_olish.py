bd0c951e1d85fc771ecef23722cd0caff3a600d0
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-31 22:30:50 +0500

    Add: 6) pop(0): birinchi elementni olish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List___asosiy_metodlar__append__insert__remove__po/6__pop_0___birinchi_elementni_olish.py
@@ -0,0 +1,9 @@
+n = int(input())
+lst = list(map(int, input().split()))
+x = lst.pop(0)
+print(x)
+print(lst)
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Birinchi elementni pop(0) qiling va chiqar.
+# Keyin listni chiqar.
