52f5732dc0303221141326371ee2b2608d3f24bb
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-31 22:30:41 +0500

    Add: 2) insert: boshiga qo‘shish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List___asosiy_metodlar__append__insert__remove__po/2__insert__boshiga_qo_shish.py
@@ -0,0 +1,9 @@
+n = int(input())
+lst = list(map(int, input().split()))
+val = int(input())
+lst.insert(0, val)
+print(lst)
+# n = int(input())
+# lst = list(map(int, input().split()))
+# val = int(input())
+# Boshiga val ni insert qiling va listni chiqaring.
