9da35d143fa93a56c264b0eb491d83b7c79eb16b
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-31 22:30:49 +0500

    Add: 5) pop: oxirgi elementni olish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List___asosiy_metodlar__append__insert__remove__po/5__pop__oxirgi_elementni_olish.py
@@ -0,0 +1,9 @@
+n = int(input())
+lst = list(map(int, input().split()))
+x = lst.pop()
+print(x)
+print(lst)
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Oxirgi elementni pop qiling va chiqar.
+# Keyin qolgan listni ham chiqar.
