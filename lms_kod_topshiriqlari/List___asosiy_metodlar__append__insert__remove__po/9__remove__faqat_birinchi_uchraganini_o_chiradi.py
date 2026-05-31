54f59098e9e6f3adfbb043b530c3c02986b78b9c
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-31 22:31:00 +0500

    Add: 9) remove: faqat birinchi uchraganini o‘chiradi [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List___asosiy_metodlar__append__insert__remove__po/9__remove__faqat_birinchi_uchraganini_o_chiradi.py
@@ -0,0 +1,9 @@
+n = int(input())
+lst = list(map(int, input().split()))
+val = int(input())
+lst.remove(val)
+print(lst)
+# n = int(input())
+# lst = list(map(int, input().split()))
+# val = int(input())
+# remove(val) qiling va listni chiqaring.
