3190ac2d8c92afd9583179fff94135580e1a682c
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-31 22:30:46 +0500

    Add: 4) remove: qiymat bo‘yicha o‘chirish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List___asosiy_metodlar__append__insert__remove__po/4__remove__qiymat_bo_yicha_o_chirish.py
@@ -0,0 +1,9 @@
+n = int(input())
+lst = list(map(int, input().split()))
+val = int(input())
+lst.remove(val)
+print(lst)
+# n = int(input())
+# lst = list(map(int, input().split()))
+# val = int(input())
+# val ni remove qiling va listni chiqaring.
