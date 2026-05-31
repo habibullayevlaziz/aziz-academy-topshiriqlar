be2b20ecd53ecaef2cc96aaa72268d546cb839fd
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-31 22:30:44 +0500

    Add: 3) insert: o‘rtaga qo‘shish [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List___asosiy_metodlar__append__insert__remove__po/3__insert__o_rtaga_qo_shish.py
@@ -0,0 +1,9 @@
+n = int(input())
+lst = list(map(int, input().split()))
+val = int(input())
+lst.insert(1, val)
+print(lst)
+# n = int(input())
+# lst = list(map(int, input().split()))
+# val = int(input())
+# 1-indexga val ni insert qiling va listni chiqaring.
