b3fb8205d02e0d05106f58a004bc7f45c8abe970
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-06-01 00:01:58 +0500

    Add: 10) Copy: list() bilan [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List___slicing_va_copy__list___va_____/10__Copy__list___bilan.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = list(map(int, input().split()))
+val = int(input())
+b = list(a)
+b.append(val)
+print(a)
+print(b)
+
+# n = int(input())
+# a = list(map(int, input().split()))
+# val = int(input())
+# b = list(a)
+# b oxiriga val qo'shing.
+# a va b ni chiqaring.
