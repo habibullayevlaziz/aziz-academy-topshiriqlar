2836879a649b37d26b848135e368b07e6cf3d1ae
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-05-31 22:30:58 +0500

    Add: 8) insert: oxiriga qo‘shish (index orqali) [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List___asosiy_metodlar__append__insert__remove__po/8__insert__oxiriga_qo_shish__index_orqali_.py
@@ -0,0 +1,9 @@
+n = int(input())
+lst = list(map(int, input().split()))
+val = int(input())
+lst.insert(len(lst), val)
+print(lst)
+# n = int(input())
+# lst = list(map(int, input().split()))
+# val = int(input())
+# insert(len(lst), val) bilan oxiriga qo'shing va listni chiqaring.
