50dc3476ad5473d47ccdff92a73a03880525d915
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-06-01 00:02:17 +0500

    Add: 14) Har ikkinchi elementni o‘chirish (slicing orqali) [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List___slicing_va_copy__list___va_____/14__Har_ikkinchi_elementni_o_chirish__slicing_orqa.py
@@ -0,0 +1,6 @@
+n = int(input())
+lst = list(map(int, input().split()))
+print(lst[::2])
+# n = int(input())
+# lst = list(map(int, input().split()))
+# Faqat juft indexdagilarni qoldiring (0,2,4...) slicing bilan va listni chiqaring.
