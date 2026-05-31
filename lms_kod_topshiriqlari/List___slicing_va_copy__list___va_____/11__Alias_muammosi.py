b08ca68cb9dbe9395c672b338e71cf7185c9fb32
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-06-01 00:02:05 +0500

    Add: 11) Alias muammosi [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/List___slicing_va_copy__list___va_____/11__Alias_muammosi.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = list(map(int, input().split()))
+b = a
+b.append(3)
+print(a)
+# n = int(input())
+# a = list(map(int, input().split()))
+# b = a  (alias)
+# b ga 3 ni append qiling.
+# a ni chiqaring (alias ekani ko'rinadi).
