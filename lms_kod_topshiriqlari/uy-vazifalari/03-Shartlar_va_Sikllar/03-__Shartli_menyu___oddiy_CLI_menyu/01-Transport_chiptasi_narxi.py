d6abeefeb5ca069edb19358da6f99499c8c35192
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-07-03 10:10:18 +0500

    Add: Transport chiptasi narxi [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-Transport_chiptasi_narxi.py
@@ -0,0 +1,11 @@
+turi = int(input())
+miqdori = int(input())
+if turi == 1:
+    narx = 1700
+elif turi == 2:
+    narx = 0
+elif turi == 3:
+    narx = 1000
+else:
+    narx = 0
+print(narx * miqdori)
