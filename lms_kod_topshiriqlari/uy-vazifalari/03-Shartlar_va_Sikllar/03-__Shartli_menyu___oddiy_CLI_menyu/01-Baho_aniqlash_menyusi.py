c3bd83e839d6328eae0094600880f0d6c6f38eab
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-07-04 15:56:42 +0500

    Add: Baho aniqlash menyusi [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-Baho_aniqlash_menyusi.py
@@ -0,0 +1,15 @@
+tasks_status = int(input())
+score = int(input())
+if tasks_status == 2:
+    print("Yiqildi")
+else:
+    if score >= 90:
+        print("A")
+    elif score >= 80:
+        print("B")
+    elif score >= 70:
+        print("C")
+    elif score >= 60:
+        print("D")
+    else:
+        print("F")
