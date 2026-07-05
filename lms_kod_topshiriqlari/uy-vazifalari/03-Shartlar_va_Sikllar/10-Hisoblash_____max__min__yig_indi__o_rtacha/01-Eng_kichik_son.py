7f3c13944acf14fd4a851dd954e2c47a777de9cb
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-07-06 00:08:44 +0500

    Add: Eng kichik son [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/10-Hisoblash_____max__min__yig_indi__o_rtacha/01-Eng_kichik_son.py
@@ -0,0 +1,11 @@
+import sys
+def main():
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+    n = int(input_data[0])
+    sonlar = [int(x) for x in input_data[1:n+1]]
+    if sonlar:
+        print(min(sonlar))
+if __name__ == '__main__':
+    main()
