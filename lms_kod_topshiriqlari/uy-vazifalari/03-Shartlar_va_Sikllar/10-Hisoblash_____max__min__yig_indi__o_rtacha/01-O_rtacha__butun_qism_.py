cae0e99f132b3b0f440ff9a0a4867df068b73ce1
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-07-06 00:59:51 +0500

    Add: O'rtacha (butun qism) [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/10-Hisoblash_____max__min__yig_indi__o_rtacha/01-O_rtacha__butun_qism_.py
@@ -0,0 +1,13 @@
+import sys
+def main():
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+    n = int(input_data[0])
+    sonlar = [int(x) for x in input_data[1:n+1]]
+    if sonlar:
+        ortacha = sum(sonlar) // n
+        print(ortacha)
+if __name__ == '__main__':
+    main()
+    
