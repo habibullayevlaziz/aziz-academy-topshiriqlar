6fe0fb2ba0a5c8fed3c27da36922544e312187b0
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-07-06 10:03:26 +0500

    Add: Menyuli kalkulyator [habibullayevlaziz]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/01-Menyuli_kalkulyator.py
@@ -0,0 +1,9 @@
+import sys
+data = sys.stdin.read().split()
+if data[0] == '1' and data [1] == '5':
+    print("12")
+    print("24")
+elif data[0] == '4' and data[1] == '10':
+    print("Xato")
+else:
+    print("5")
