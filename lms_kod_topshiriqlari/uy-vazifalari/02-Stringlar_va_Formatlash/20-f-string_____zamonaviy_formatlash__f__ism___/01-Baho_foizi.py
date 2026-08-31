40cf628a04db3624c1455bce23ec1de09ec59f33
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-08-31 08:12:07 +0500

    Add: Baho foizi [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/20-f-string_____zamonaviy_formatlash__f__ism___/01-Baho_foizi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/20-f-string_____zamonaviy_formatlash__f__ism___/01-Baho_foizi.py
@@ -1,4 +1,5 @@
-a = int(input())
-b = int(input())
-print(f"Natija: {a}/{b}")
-print(f"Foiz: {a/b*100}%")
+bal = int(input())
+maksimal = int(input())
+foiz = bal / maksimal * 100
+print(f"Natija: {bal}/{maksimal}")
+print(f'Foiz: {foiz:.1f}%')
