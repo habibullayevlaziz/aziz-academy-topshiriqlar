15a69c00951a34894c9e1e3690613f56adadda35
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-03 21:24:10 +0500

    Add: Unli harflar soni [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/24-__Amaliy__Matn_tahlili___so_z_sanash__harf_statist/01-Unli_harflar_soni.py
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/24-__Amaliy__Matn_tahlili___so_z_sanash__harf_statist/01-Unli_harflar_soni.py
@@ -1,7 +1,3 @@
-matn = input()
-unli_harflar = "aeiouAEIOU"
-sanoq = 0
-for harf in matn:
-    if harf in unli_harflar:
-        sanoq += 1
-print(sanoq)
+matn = input().lower()
+jami = matn.count('a') + matn.count('e') + matn.count('i') + matn.count('o') + matn.count('u')
+print(jami)
