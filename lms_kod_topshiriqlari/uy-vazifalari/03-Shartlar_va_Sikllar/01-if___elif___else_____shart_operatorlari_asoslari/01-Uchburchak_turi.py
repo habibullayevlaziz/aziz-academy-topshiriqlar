617bd0354d682931e031ce4de5b9c24cc2570d6d
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-19 14:21:18 +0500

    Add: Uchburchak turi [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Uchburchak_turi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/01-if___elif___else_____shart_operatorlari_asoslari/01-Uchburchak_turi.py
@@ -1,7 +1,11 @@
-tomonlar = {int(input()), int(input()), int(input())}
-if len(tomonlar) == 1:
+a = int(input())
+b = int(input())
+c = int(input())
+if a <= 0 or b <= 0 or c <= 0 or (a + b <= c) or (a + c <= b) or (b + c <= a):
+    print("notogri")
+elif a == b == c:
     print("teng tomonli")
-elif len(tomonlar) == 2:
+elif a == b or a == c or b == c:
     print("teng yonli")
 else:
     print("turli tomonli")
