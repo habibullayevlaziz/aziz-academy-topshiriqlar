0146b4a0b07110623155060bb4a0bfaf2f47870e
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-20 09:12:12 +0500

    Add: Kabisa yilni aniqlash [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Kabisa_yilni_aniqlash.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Kabisa_yilni_aniqlash.py
@@ -1,5 +1,12 @@
 yil = int(input())
-if (yil % 4 == 0 and yil % 100 != 0) or (yil % 400 == 0):
-    print("Kabisa")
+if yil % 4 == 0:
+    if yil % 100 == 0:
+        if yil % 400 == 0:
+            print("Kabisa")
+        else:
+            print("Kabisa emas")
+    else:
+        print("Kabisa")
 else:
     print("Kabisa emas")
+    
