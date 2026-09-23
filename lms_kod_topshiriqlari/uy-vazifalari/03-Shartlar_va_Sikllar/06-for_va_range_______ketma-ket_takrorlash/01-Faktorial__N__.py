cd1d1ba305aa6b65c2d853261b208358e54ed098
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-23 16:18:30 +0500

    Add: Faktorial (N!) [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/06-for_va_range_______ketma-ket_takrorlash/01-Faktorial__N__.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/06-for_va_range_______ketma-ket_takrorlash/01-Faktorial__N__.py
@@ -1,10 +1,3 @@
-import sys
 import math
-def main():
-    input_data = sys.stdin.read().split()
-    if not input_data:
-        return
-    n = int(input_data[0])
-    print(math.factorial(n))
-if __name__ == '__main__':
-    main()
+n = int(input())
+print(math.factorial(n))
