255622b9c1718ad81d3303ddfee9de0df0a07b85
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-23 16:11:21 +0500

    Add: Tub sonlar sanog'i [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Tub_sonlar_sanog_i.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Tub_sonlar_sanog_i.py
@@ -1,15 +1,16 @@
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+import sys
 count = 0
-while True:
-    n = int(input())
+for line in sys.stdin.read().split():
+    n = int(line)
     if n == 0:
         break
-    if is_prime(n):
+    if n < 2:
+        continue
+    is_prime = True
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+    if is_prime:
         count += 1
 print(count)
