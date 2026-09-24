3619606bc2a28a30d6f84c1cfd3a77a1632a4dba
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-24 06:55:42 +0500

    Add: Unli harflar soni [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/07-for_bilan_kolleksiyalar___string_list_bo_ylab_yuri/01-Unli_harflar_soni.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/07-for_bilan_kolleksiyalar___string_list_bo_ylab_yuri/01-Unli_harflar_soni.py
@@ -1,7 +1,6 @@
-text = input()
-vowels = "aeiouAEIOU"
-counter = 0
-for char in text:
-    if char in vowels:
-        counter += 1
-print(counter)
+soz = input()
+unli_soni = 0
+for ch in soz:
+    if ch in "aeiou":
+        unli_soni += 1
+print(unli_soni)
