c44cbc99363249b0c6f6ef6b46bd7b2d568e58f0
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-24 06:45:07 +0500

    Add: 7 ga bo'linadigan birinchi son [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-7_ga_bo_linadigan_birinchi_son.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-7_ga_bo_linadigan_birinchi_son.py
@@ -1,10 +1,8 @@
 n = int(input())
-found = False
 for _ in range(n):
     son = int(input())
     if son % 7 == 0:
         print(son)
-        found = True
         break
-if not found:
+else:
     print("yo'q")
