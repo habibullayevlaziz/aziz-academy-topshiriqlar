c377e591f311c1095d7ba02ed37fdfceb0408844
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-24 07:04:23 +0500

    Add: Musbat sonlar yig'indisi [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/09-Filtrlash___juft_sonlar__musbat_sonlar_ajratish/01-Musbat_sonlar_yig_indisi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/09-Filtrlash___juft_sonlar__musbat_sonlar_ajratish/01-Musbat_sonlar_yig_indisi.py
@@ -1,14 +1,7 @@
-import sys
-def main():
-    input_data = sys.stdin.read().split()
-    if not input_data:
-        return
-    n = int(input_data[0])
-    musbat_yigindi = 0
-    for i in range(1, n + 1):
-        number = int(input_data[i])
-        if number > 0:
-            musbat_yigindi += number
-    print(musbat_yigindi)
-if __name__ == '__main__':
-    main()
+n = int(input())
+yigindi = 0
+for _ in range(n):
+    son = int(input())
+    if son > 0:
+        yigindi += son
+print(yigindi)
