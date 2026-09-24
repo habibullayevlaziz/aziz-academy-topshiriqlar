916a9d5b6b403c39f8574daad4a319d03fa9b50c
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-24 06:47:00 +0500

    Add: Shartli yig'indi [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Shartli_yig_indi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Shartli_yig_indi.py
@@ -1,10 +1,9 @@
 yigindi = 0
 while True:
     son = int(input())
-    if son == 0:
+    if son == 0 or son >= 100:
         break
-    if son >= 100:
-        break
-    if son > 0:
-        yigindi += son
+    if son < 0:
+        continue
+    yigindi += son
 print(yigindi)
