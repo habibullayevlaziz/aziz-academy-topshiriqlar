40805a4970a374a4bb6af1c012d35182472772d8
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-26 00:38:08 +0500

    Add: To'liq ko'paytirish jadvali [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/11-Nested_loop___ko_paytirish_jadvali/01-To_liq_ko_paytirish_jadvali.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/11-Nested_loop___ko_paytirish_jadvali/01-To_liq_ko_paytirish_jadvali.py
@@ -1,13 +1,3 @@
-import sys
-def main():
-    input_data = sys.stdin.read().strip()
-    if not input_data:
-        return
-    n = int(input_data)
-    for i in range(1, n + 1):
-        row = []
-        for j in range(1, n + 1):
-            row.append(str(i * j))
-        print(" ".join(row))
-if __name__ == "__main__":
-    main()
+n = int(input())
+for i in range(1, n + 1):
+    print(*(i * j for j in range(1, n + 1)))
