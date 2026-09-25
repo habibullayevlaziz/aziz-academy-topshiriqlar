25d0ab85f24d54197101afeab8a27717aaa48bb0
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-26 00:41:44 +0500

    Add: Faqat juft ko'paytmalar [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/11-Nested_loop___ko_paytirish_jadvali/01-Faqat_juft_ko_paytmalar.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/11-Nested_loop___ko_paytirish_jadvali/01-Faqat_juft_ko_paytmalar.py
@@ -1,12 +1,5 @@
-import sys
-def main():
-    input_data = sys.stdin.read().strip()
-    if not input_data:
-        return
-    n = int(input_data)
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            if i == 2 or j == 2:
-                print(f"{i} x {j} = {i * j}")
-if __name__ == "__main__":
-    main()
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if (i * j) % 2 == 0:
+            print(f"{i} x {j} = {i * j}")
