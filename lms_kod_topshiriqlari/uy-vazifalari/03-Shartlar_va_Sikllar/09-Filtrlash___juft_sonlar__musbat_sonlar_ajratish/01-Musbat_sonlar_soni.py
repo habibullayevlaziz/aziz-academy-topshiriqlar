76cee4ff753488ffca46466cd0c551c6a407868f
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-25 14:20:35 +0500

    Add: Musbat sonlar soni [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/09-Filtrlash___juft_sonlar__musbat_sonlar_ajratish/01-Musbat_sonlar_soni.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/09-Filtrlash___juft_sonlar__musbat_sonlar_ajratish/01-Musbat_sonlar_soni.py
@@ -1,14 +1,7 @@
-import sys
-def main():
-    input_data = sys.stdin.read().split()
-    if not input_data:
-        return
-    n = int(input_data[0])
-    musbat_lar_soni = 0
-    for i in range(1, n + 1):
-        number = int(input_data[i])
-        if number > 0:
-            musbat_lar_soni += 1
-    print(musbat_lar_soni)
-if __name__ == '__main__':
-    main()
+n = int(input())
+musbat_son = 0
+for _ in range(n):
+    son = int(input())
+    if son > 0:
+        musbat_son += 1
+print(musbat_son)
