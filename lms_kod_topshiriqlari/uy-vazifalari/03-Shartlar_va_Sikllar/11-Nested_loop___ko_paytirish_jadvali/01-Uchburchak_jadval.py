3595efe2e4e136eef59f73b5185b54993e5e39e4
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-25 14:24:19 +0500

    Add: Uchburchak jadval [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/11-Nested_loop___ko_paytirish_jadvali/01-Uchburchak_jadval.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/11-Nested_loop___ko_paytirish_jadvali/01-Uchburchak_jadval.py
@@ -1,6 +1,4 @@
-N = int(input())
-for i in range(1, N + 1):
-    qator = []
-    for j in range(1, i + 1):
-        qator.append(str(i * j))
+n = int(input())
+for i in range(1, n + 1):
+    qator = [str(i * j) for j in range(1, i + 1)]
     print(" ".join(qator))
