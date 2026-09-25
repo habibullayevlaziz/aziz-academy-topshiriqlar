f65a9fadba80d8b01fcc1da0c7b86bdf7de9b781
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-25 23:48:43 +0500

    Add: n gacha tub sonlar [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/01-n_gacha_tub_sonlar.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/12-Oddiy_algoritmlar___bo_luvchilar__tub_son_tekshiri/01-n_gacha_tub_sonlar.py
@@ -2,12 +2,11 @@ n = int(input())
 primes = []
 for num in range(2, n + 1):
     is_prime = True
-    i = 2
-    while i * i <= num:
+    limit = int(num ** 0.5) + 1
+    for i in range(2, limit):
         if num % i == 0:
             is_prime = False
             break
-        i += 1
     if is_prime:
-        primes.append(num)
-print(*(primes))
+            primes.append(num)
+print(*primes)
