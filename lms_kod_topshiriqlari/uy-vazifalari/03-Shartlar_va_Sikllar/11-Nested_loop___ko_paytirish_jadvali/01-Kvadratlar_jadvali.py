dfaa14585ca98f05420b7f663dd981ad6692ebef
habibullayevlaziz <habibullayevlaziz9@gmail.com>
2026-09-26 00:34:44 +0500

    Add: Kvadratlar jadvali [habibullayevlaziz]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/11-Nested_loop___ko_paytirish_jadvali/01-Kvadratlar_jadvali.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/11-Nested_loop___ko_paytirish_jadvali/01-Kvadratlar_jadvali.py
@@ -1,10 +1,3 @@
-import sys
-def main():
-    input_data = sys.stdin.read().strip()
-    if not input_data:
-        return
-    n = int(input_data)
-    for i in range(1, n + 1):
-        print(f"{i} * {i} = {i * i}")
-if __name__ == "__main__":
-    main()
+n = int(input())
+for i in range(1, n + 1):
+    print(f"{i} * {i} = {i * i}")
